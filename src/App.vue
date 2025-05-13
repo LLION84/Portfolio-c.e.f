e737084995fef97dc5fe408d660fcde4da45d6f4
BlaiseMohamed <blaisemohamed@outlook.fr>
2025-05-13 20:42:50 +0200

    Ajout de la section présentation et configuration des polices

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,21 @@
 import Home from "./component/Home.vue";
 </script>
 
-<style></style>
+<style>
+body {
+  font-family: "Inter", sans-serif; /* Texte principal */
+  margin: 0;
+  padding: 0;
+  color: #333;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Poppins", sans-serif; /* Titres */
+  margin: 0;
+}
+</style>
--- src/component/Home.vue
+++ src/component/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <Header />
+    <main>
+      <section id="about-me">
+        <h1>Blaise Zinou محمد</h1>
+
+        <p>
+          Je m'appelle Zinou, je suis né à Marseille, la ville de la sardine. Je
+          me suis reconverti dans le développement après avoir obtenu mon bac en
+          systèmes numériques, un domaine qui restait encore flou pour moi à
+          l'époque. Il m’a paru essentiel de travailler dans l'informatique, un
+          secteur qui offre à la fois liberté professionnelle et forte
+          croissance. C’est ainsi que j’ai découvert le codage, une discipline
+          passionnante qui semblait apporter des réponses à toutes mes attentes.
+        </p>
+      </section>
+    </main>
   </div>
 </template>
 
